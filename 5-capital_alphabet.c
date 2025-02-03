90ceaef5d5c4eee5479c647a83f28710c3f4f7a6
Darshan  Dave <darshandave637@gmail.com>
2025-02-03 22:49:20 +0530

    Add files via upload

---
+++ 5-capital_alphabet.c
@@ -0,0 +1,19 @@
+/*
+
+A B C D E...Z
+Ascii value has started 65 to 90
+
+*/
+#include<stdio.h>
+void main()
+{
+	int i,number;
+	
+	printf("enter positive number :");
+	scanf("%d",&number);
+	
+	for(i=1;i<=number;i++)
+	{
+		printf("%c ",i+64);
+	}
+}
