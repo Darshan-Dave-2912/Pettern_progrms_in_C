69e15a38eb5c4569c9f965c3530df303a3da3bce
Darshan  Dave <darshandave637@gmail.com>
2025-02-03 22:48:57 +0530

    Add files via upload

---
+++ 4-star.c
@@ -0,0 +1,19 @@
+/*
+
+* * * * *...n
+
+*/
+
+#include<stdio.h>
+void main()
+{
+	int i,number;
+	
+	printf("enter positive number :");
+	scanf("%d",&number);
+	
+	for(i=1;i<=number;i++)
+	{
+		printf("* ");
+	}
+}
