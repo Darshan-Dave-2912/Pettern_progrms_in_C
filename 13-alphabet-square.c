407a00972d665ef1d0f94aa5570678c392f4c4f4
Darshan  Dave <darshandave637@gmail.com>
2025-02-07 23:22:32 +0530

    Add files via upload

---
+++ 13-alphabet-square.c
@@ -0,0 +1,26 @@
+/*
+
+A A A A A
+B B B B B
+C C C C C
+D D D D D
+E E E E E
+
+*/
+#include<stdio.h>
+void main()
+{
+	int row, number, column;
+	
+	printf("enter positive number :");
+	scanf("%d",&number);
+	
+	for(row=1; row<=number; row++)
+	{
+		for(column=1; column<=number; column++)
+		{
+			printf("%c ",row+64);
+		}
+		printf("\n");
+	}
+}
