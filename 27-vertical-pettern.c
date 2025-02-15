47c36f2e637a0d49a3372e0397b48b1bfd657f46
Darshan  Dave <darshandave637@gmail.com>
2025-02-15 21:08:17 +0530

    Add files via upload

---
+++ 27-vertical-pettern.c
@@ -0,0 +1,26 @@
+/*
+
+1 1 1 1 1
+2 2 2 2
+3 3 3
+4 4
+5
+
+*/
+#include<stdio.h>
+void main()
+{
+	int row, number, column;
+	
+	printf("enter positive number :");
+	scanf("%d",&number);
+	
+	for(row=1; row<=number; row++)
+	{
+		for(column=number; column>=row; column--)
+		{
+			printf("%d ",row);
+		}
+		printf("\n");
+	}
+}
