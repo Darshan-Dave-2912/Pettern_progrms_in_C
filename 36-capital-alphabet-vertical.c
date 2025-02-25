5147609e428963521ecef19f955f2113dd62fb02
Darshan  Dave <darshandave637@gmail.com>
2025-02-25 22:10:34 +0530

    capital alphabet vertical pattern

---
+++ 36-capital-alphabet-vertical.c
@@ -0,0 +1,29 @@
+/*
+        A
+      A B
+    A B C
+  A B C D
+A B C D E
+
+*/
+#include<stdio.h>
+void main()
+{
+	int row, column, space, number;
+	
+	printf("enter positive number :");
+	scanf("%d",&number);
+	
+	for(row=1; row<=number; row++)
+	{
+		for(space=number-1; space>=row; space--)
+		{
+			printf("  ");
+		}
+		for(column=1; column<=row; column++)
+		{
+			printf("%c ",column+64);
+		}
+		printf("\n");
+	}
+}
